7656f6852474b486d4132c73f88224c910f5ac79
erixweb <erikmorenodev@gmail.com>
2023-03-12 22:16:31 +0100

    first commit

---
+++ component/aside.js
@@ -0,0 +1,7 @@
+export function getAsideProps () {
+    return [
+        '<a href="/docs/paddings-margins">Paddings & Margins</a>',
+        '<a href="/docs/width-height">Width & Height</a>',
+        '<a href="/docs/backgrounds">Backgrounds</a>',
+    ]
+}
