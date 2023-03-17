2868ca80fb466f32dc90f4ee3158c8d43a7c3825
erixweb <erikmorenodev@gmail.com>
2023-03-17 20:23:11 +0100

    ➕Más documentación & Cache

--- component/aside.js
+++ component/aside.js
@@ -3,5 +3,6 @@ export function getAsideProps () {
         '<a href="/docs/paddings-margins">Paddings & Margins</a>',
         '<a href="/docs/width-height">Width & Height</a>',
         '<a href="/docs/backgrounds">Backgrounds</a>',
+        '<a href="/docs/borders">Border colors & Radius</a>',
     ]
 }
