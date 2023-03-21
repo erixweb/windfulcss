e951bbe9e38a159fba9b910057416cc0f89fe680
erixweb <erikmorenodev@gmail.com>
2023-03-21 19:19:09 +0100

    🍃Font-Color & Size

--- component/aside.js
+++ component/aside.js
@@ -3,6 +3,7 @@ export function getAsideProps () {
         '<a href="/docs/paddings-margins">Paddings & Margins</a>',
         '<a href="/docs/width-height">Width & Height</a>',
         '<a href="/docs/backgrounds">Backgrounds</a>',
+        '<a href="/docs/fonts">Font Family & Colors</a>',
         '<a href="/docs/borders">Border colors & Radius</a>',
     ]
 }
